3eea34d9b6f72fd56c8b14b9b8ead0ea4fe65497
ksilin <konstantin.silin@gmail.com>
2017-05-30 17:55:32 +0200

    drawing first car on canvas

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const ctx = document.getElementById("canvas").getContext("2d")
+
 class Car {
     constructor(id, genes) {
         this.id = id;
@@ -25,6 +27,14 @@ class Car {
         this.wheelVertex1 = genes[19];
         this.axleAngle1 = genes[20];
         this.wheelRadius1 = genes[21];
+
+        this.lengths = Object.keys(this).filter(key => key.includes("cartMag")).map(key => {
+            return {key, len: this[key]}
+        })
+
+        this.angles = Object.keys(this).filter(key => key.includes("cartAngle")).map(key => {
+            return {key, angle: this[key]}
+        })
     }
 }
 
@@ -48,7 +58,7 @@ function getRandomInt(min, max) {
 }
 
 function getRandomElement(ar, from = 0) {
-  return ar[getRandomInt(from, ar.length - 1)]
+    return ar[getRandomInt(from, ar.length - 1)]
 }
 
 class PopulationGenerator {
@@ -106,44 +116,100 @@ class Evolver {
         return new Car(generateId(20), genes)
     }
 
-  // [{ car: Car(...), fitness: 0.1}]
+    // [{ car: Car(...), fitness: 0.1}]
     truncate(carsAndFitness, rate = 0.9) {
-      carsAndFitness.sort((a, b) => b.fitness - a.fitness )
-      return carsAndFitness.slice(0, carsAndFitness.length * rate)
+        carsAndFitness.sort((a, b) => b.fitness - a.fitness)
+        return carsAndFitness.slice(0, carsAndFitness.length * rate)
     }
 
 }
 
+class Geo {
+
+  pointFromOriginAngleLength(origin, angle, length) {
+      return {
+          x: origin.x + Math.cos(angle) * length,
+          y: origin.y + Math.sin(angle) * length
+      }
+  }
+
+  createTriangle(center, b, c) {
+      console.log("creatingTriangle");
+      ctx.beginPath();
+      ctx.moveTo(center.x, center.y);
+      ctx.lineTo(b.x, b.y);
+      ctx.lineTo(c.x, c.y);
+      ctx.fill();
+  }
+}
+
 class Main {
     constructor() {
+
+        // this.createTriangle = this.createTriangle.bind(this)
+
         this.populationGen = new PopulationGenerator()
         this.population = this.populationGen.generatePopulation(10)
 
         this.evolver = new Evolver()
 
         const carsWithFitness = this.population.map(car => {
-          return { car, "fitness": this.evolver.fitness(car) }
+            return {car, "fitness": this.evolver.fitness(car)}
         })
 
         // this.evolver.mutate(this.population[0])
         const truncated = this.evolver.truncate(carsWithFitness, 0.7)
-        console.log(truncated.map(c => c.car.id));
         const matingPool = this.evolver.tournament(this.population)
-        console.log(matingPool[0]);
-        console.log(matingPool[1]);
 
         const child1 = this.evolver.crossover(getRandomElement(matingPool), getRandomElement(matingPool))
         const child2 = this.evolver.crossover(getRandomElement(matingPool), getRandomElement(matingPool))
 
         const mutant = this.evolver.mutate(getRandomElement(truncated, 1).car)
 
-       let newGen = []
-       newGen = newGen.concat(truncated.map(elem => elem.car))
-       newGen.push(child1)
-       newGen.push(child2)
-       newGen.push(mutant)
-       console.log(newGen.map(c => c.id))
+        let newGen = []
+        newGen = newGen.concat(truncated.map(elem => elem.car))
+        newGen.push(child1)
+        newGen.push(child2)
+        newGen.push(mutant)
+        console.log(newGen.map(c => c.id))
     }
+
+    run() {
+        const car = this.population[0]
+        console.log(car);
+
+        const lengths = car.lengths.sort((a, b) => a.key.localeCompare(b.key))
+        const angles = car.angles.sort((a, b) => a.key.localeCompare(b.key))
+
+        var carGeoSortedByAngle = angles.map(function(e, i) {
+            return Object.assign(e, lengths[i]);
+        }).sort((a, b) => a.angle - b.angle);
+
+        const geo = new Geo()
+        const center = {
+            x: 400,
+            y: 300
+        }
+
+        const lengthFactor = 50
+
+        const withPoints = carGeoSortedByAngle.map(function(e, i) {
+            e.point = geo.pointFromOriginAngleLength(center, e.angle, e.len * lengthFactor)
+            return e
+        })
+
+        const newPoint1 = geo.pointFromOriginAngleLength(center, carGeoSortedByAngle[0].angle, carGeoSortedByAngle[0].len * lengthFactor)
+        const newPoint2 = geo.pointFromOriginAngleLength(center, carGeoSortedByAngle[3].angle, carGeoSortedByAngle[3].len * lengthFactor)
+
+        withPoints.map(function(e, i) {
+            const nextIdx = (i + 1) % (withPoints.length - 1)
+            const nextElem = withPoints[nextIdx]
+            geo.createTriangle(center, e.point, nextElem.point)
+        })
+
+    }
+
 }
 
 const m = new Main();
+m.run();
